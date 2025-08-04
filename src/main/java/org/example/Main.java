c0ceb1bbdebd3f09844d01e9f932285193fa2a1c
MzaCoding <nyikogiven74@gmail.com>
2025-08-05 00:13:32 +0200

    update tesseract dependencies

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,27 @@
 package org.example;
 
 
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
 
 
-         System.err.println("Welcome Back Nyiko");
+         Tesseract tesseract =new Tesseract();
+
+
+        tesseract.setDatapath("C://Program Files//Tess4J//tessdata");
+
+        String extractedText = null;
+        try {
+            extractedText = tesseract.doOCR(new File("C://Users//nyiko//Downloads//R.jpg"));
+        } catch (TesseractException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(extractedText);
 
     }
 }
